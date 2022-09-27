d545075436866579fd630dd2f2d40187c61cbad0
gammelalf <gammelalf@hopfen.space>
2022-09-27 23:23:38 +0200

    Removed wasm binding for now

--- wasm/playground/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-WASM=./pkg/nbezier_wasm.js
-if [ -f "$WASM" ]; then
-    echo "Visit http://0.0.0.0:8000/playground/"
-    /usr/bin/env python3 -m http.server
-else 
-    echo "Please run from inside the wasm crate root"
-    echo "Make sure you have compiled it using wasm-pack --target web"
-fi
